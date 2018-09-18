452488a99a72a8a2dff7079fc5a9af7788644818
Mazinov Osman <osman.mazinov@arsis.ru>
2018-09-18 17:57:40 +0300

    Prototype pattern has been refactored and tested

---
+++ Prototype/IFigure.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prototype
+{
+    public interface IFigure
+    {
+        IFigure Clone();
+        void GetInfo();
+    }
+}
