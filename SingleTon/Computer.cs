d6270f02ff99b2e31c8d3884405583e60edbcdb9
Mazinov Osman <osman.mazinov@arsis.ru>
2018-09-19 11:05:30 +0300

    Singleton refactored and diagram added

---
+++ SingleTon/Computer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingleTon
+{
+    class Computer
+    {
+        public OpSystem Os { get; set; }
+
+        public void Launch(string osName)
+        {
+            Os = OpSystem.GetInstance(osName);
+        }
+    }
+}
