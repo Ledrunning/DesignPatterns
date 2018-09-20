9fc38b460e8c265a9e8d2eca981142283522ffba
Mazinov Osman <osman.mazinov@arsis.ru>
2018-09-20 15:42:50 +0300

    Project Memento and diagram shape added

---
+++ Memento/GameHistory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Memento
+{
+    /// <summary>
+    /// Caretaker
+    /// </summary>
+    public class GameHistory
+    {
+        public Stack<HeroMemento> History { get; private set; }
+
+        public GameHistory()
+        {
+            History = new Stack<HeroMemento>();
+        }
+    }
+}
