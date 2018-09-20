cc21c21b4f8eef90783c345b8f5dfd475e96747c
Mazinov Osman <osman.mazinov@arsis.ru>
2018-09-20 11:06:06 +0300

    Project with Interpreter pattern added

---
+++ Interpreter/NumberExpression.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interpreter
+{
+    class NumberExpression : IExpression
+    {
+        private string _name;
+
+        public NumberExpression(string variableName)
+        {
+            _name = variableName;
+        }
+
+        public int Interpret(Context context)
+        {
+            return context.GetVariable(_name);
+        }
+    }
+}
