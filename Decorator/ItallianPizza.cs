4f848abb76b577c24eb5e1405ca66b05ae215c99
Mazinov Osman <osman.mazinov@arsis.ru>
2018-09-24 10:45:46 +0300

    Project decorator added

---
+++ Decorator/ItallianPizza.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    public class ItallianPizza : Pizza
+    {
+
+        public ItallianPizza() : base("Итальянская пица")
+        {
+
+        }
+
+        public override int GetCost() => 10;
+    }
+}
---
+++ Decorator/TomatoPizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator
+{
+    class TomatoPizza : PizzaDecorator
+    {
+        public TomatoPizza(Pizza pizza) : base(pizza.Name + ", с томатами", pizza)
+        {
+
+        }
+
+        public override int GetCost() => pizza.GetCost() + 3;
+    }
+}
