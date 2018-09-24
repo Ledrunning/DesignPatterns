ef122812c6e07611a3b9baf4d9f2a56f085528e8
Mazinov Osman <osman.mazinov@arsis.ru>
2018-09-24 09:19:54 +0300

    Project with implemented pattern visitor added

---
+++ Visitor/Company.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    public class Company : IAccount
+    {
+        public string Name { get; set; }
+        public string RegNumber { get; set; }
+        public string Number { get; set; }
+
+        public void ToHtml()
+        {
+            string result = "<table><tr><td>Свойство<td><td>Значение</td></tr>";
+            result += $"<tr><td>Name<td><td>{Name}</td></tr>";
+            result += $"<tr><td>RegNumber<td><td>{RegNumber}</td></tr>";
+            result += $"<tr><td>Number<td><td>{Number}</td></tr></table>";
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Visitor/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Visitor
+{
+    public class Person : IAccount
+    {
+        public string Name { get; set; }
+        public string AccountNumber { get; set; }
+
+        public void ToHtml()
+        {
+            string result = @"<table><tr><td>Свойство<td><td>Значение</td></tr>";
+            result += $"<tr><td>FIO<td><td>{Name}</td></tr>";
+            result += $"<tr><td>Number<td><td>{AccountNumber}</td></tr></table>";
+            Console.WriteLine(result);
+        }
+    }
+}
