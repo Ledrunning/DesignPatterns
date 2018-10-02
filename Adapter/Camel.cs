0aa77d20df63d69b8beb9171bcf6513d39011b08
Mazinov Osman <osman.mazinov@arsis.ru>
2018-10-02 13:00:29 +0300

    Project with adaptern pattern has been added

---
+++ Adapter/Camel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Adapter.Interfaces;
+
+namespace Adapter
+{
+    public class Camel : IAnimal
+    {
+        public void Move()
+        {
+            Console.WriteLine("Верблюд идет по пустыне");
+        }
+    }
+}
---
+++ Adapter/CamelToTransportAdapter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Adapter.Interfaces;
+
+namespace Adapter
+{
+    class CamelToTransportAdapter : ITransport
+    {
+        private Camel _camel;
+
+        public CamelToTransportAdapter(Camel camel)
+        {
+            _camel = camel;
+        }
+
+        public void Drive()
+        {
+            _camel.Move();
+        }
+    }
+}
