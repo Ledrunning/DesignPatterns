f90ebf70d6adbda117259cc8f285eca4430d7b33
Mazinov Osman <osman.mazinov@arsis.ru>
2018-10-02 14:44:01 +0300

    Composite project added in base solution

---
+++ StreamRefreshTimeout/Directory.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Composite
+{
+    class Directory : Component
+    {
+        private List<Component> components = new List<Component>();
+
+        public Directory(string name) : base(name)
+        {
+
+        }
+
+        public override void Add(Component component)
+        {
+            components.Add(component);
+        }
+
+        public override void Remove(Component component)
+        {
+            components.Remove(component);
+        }
+
+        public override void Print()
+        {
+            Console.WriteLine("Узел " + name);
+            Console.WriteLine("Подузлы:");
+            for (int i = 0; i < components.Count; i++)
+            {
+                components[i].Print();
+            }
+        }
+    }
+}
