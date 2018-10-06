a480262f936d41aebe1b46f0ed3d0ebff7331a10
Mazinov Osman <osman.mazinov@arsis.ru>
2018-10-06 09:59:17 +0300

    EF Core has been added, database added. Project has been tested

---
+++ Proxy/BookStore.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Proxy
+{
+    class BookStore : IBook
+    {
+        private PageContext _dbContext;
+
+        public BookStore()
+        {
+            _dbContext = new PageContext();
+        }
+
+        public void Dispose()
+        {
+            _dbContext.Dispose();
+        }
+
+        public Page GetPage(int number) => _dbContext.Pages.FirstOrDefault(p => p.Number == number);
+
+        public void SetData(Page page)
+        {
+            _dbContext.Pages.Add(page);
+            _dbContext.SaveChanges();
+        }
+    }
+}
