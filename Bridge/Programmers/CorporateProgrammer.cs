468f9ce8592f9082e9c6ed7af38f9083c70d4521
Mazinov Osman <osman.mazinov@arsis.ru>
2018-10-06 11:17:14 +0300

    FlyWeight pattern project added

---
+++ Bridge/Programmers/CorporateProgrammer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bridge
+{
+    class CorporateProgrammer : Programmer
+    {
+        public CorporateProgrammer(ILanguage language) : base(language)
+        {
+        }
+
+        public override void EarnMoney()
+        {
+            Console.WriteLine("Получаем в конце месяца зарплату");
+        }
+    }
+}
---
+++ Flyweight/House.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flyweight
+{
+    public abstract class House
+    {
+        protected int stages; // Кл-во этажей
+
+        public abstract void Build(double longtitude, double latitude);
+    }
+}
